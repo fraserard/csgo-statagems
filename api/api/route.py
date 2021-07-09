b2d1ffce1a2dfef28bbac290a691552fd7d35a5b
Fraser Ard <fraserard@gmail.com>
2021-07-09 12:59:43 -0600

    added player crud api

--- api/api/route.py
+++ api/api/route.py
@@ -1,45 +1,49 @@
-from flask import Blueprint, request
-from .model import db, Gamer
+from flask import request
+from .model import db, Player
+from flask_restful import Resource
+from .schemas import players_schema, player_schema
 
+def initialize_routes(api):
+    api.add_resource(PlayersApi, '/api/players')
+    api.add_resource(PlayerApi, '/api/players/<id>')
 
-main_bp = Blueprint('main', __name__, url_prefix='/')
+class PlayersApi(Resource):
+    def get(self): # get all players
+        players = Player.query.all()
+        return players_schema.dump(players)
+        
+    def post(self): # add a player
+        data = request.get_json()
+        newPlayer = Player(
+            steam_id = data['steam_id'],
+            username = data['username'],
+            preferred_username = data['preferred_username'],
+            name = data['name'])
+        db.session.add(newPlayer)
+        db.session.commit()
+        return {"msg": "success"}
+        #return player_schema.dump(newPlayer)
 
-@main_bp.route('/flap')
-def flap():
-    return 'Flapper'
-
-@main_bp.route('/gamers/', methods=['GET'])
-def get_gamers():
-    gamers = Gamer.query.all()
-
-    output = []
-    for gamer in gamers:
-        gamer_data = {'username': gamer.username, 'rank': gamer.rank}
-
-        output.append(gamer_data)
-
-    return {"gamers": output}    
-
-@main_bp.route('/gamers/<id>')
-def get_gamer(id):
-    gamer = Gamer.query.get_or_404(id)
-    return {'username': gamer.username, 'rank': gamer.rank}
-
-@main_bp.route('/gamers', methods=['POST'])
-def add_gamer():
-    gamer = Gamer(
-        username = request.json['username'],
-        rank = request.json['rank'])
-    db.session.add(gamer)
-    db.session.commit()
-    return {'id': gamer.id, 'msg': 'added successfully'}
-
-@main_bp.route('/gamers/<id>', methods=['DELETE'])
-def remove_gamer(id):
-    gamer = Gamer.query.get(id)
-    if gamer is None:
-        return {"error": "gamer not found"}
-    db.session.delete(gamer)
-    db.session.commit()
-
-    return {'id': gamer.id, 'msg': 'removed successfully'}
+class PlayerApi(Resource):
+    def get(self, id): # get a player by id
+        player = Player.query.get_or_404(id)
+        return player_schema.dump(player)
+    def put(self, id): # update player by id
+        data = request.get_json()
+        player = Player.query.get()
+        if player is None:
+            return {"msg": "error",
+                    "reason": "No player found."}
+        if data['username'] is not None:
+            player.username = data['username']
+        if data['preferred_username'] is not None:
+            player.preferred_username = data['preferred_username']
+        if data['name'] is not None:
+            player.name = data['name']
+        db.session.commit()
+        return {"msg": "success"}
+    def delete(self, id): # delete a player by id
+        player = Player.query.get_or_404(id)
+        db.session.delete(player)
+        db.session.commit()
+        return {"msg": "success"}
