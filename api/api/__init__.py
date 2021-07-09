f6eb08ad847519b9b66253f5dcbd4dd50db43f82
Fraser Ard <fraserard@gmail.com>
2021-07-09 13:00:44 -0600

    updated create_app() to initialize marshmallow

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,19 +1,26 @@
 from flask import Flask
+from flask_restful import Api
+from .route import initialize_routes
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI='sqlite:///test.db',
+        SQLALCHEMY_DATABASE_URI='mysql://root:admin@localhost:3306/statagems',
+        #SQLALCHEMY_DATABASE_URI='sqlite:///test.db',
         SQLALCHEMY_TRACK_MODIFICATIONS='false'
     )
 
-    from api.model import db
+    from .model import db
     db.init_app(app)
-
-    from .route import main_bp
-    app.register_blueprint(main_bp)
+    
+    from .schemas import ma
+    ma.__init__(app)
+    
+    api = Api(app)
+    initialize_routes(api)
 
     return app
 
