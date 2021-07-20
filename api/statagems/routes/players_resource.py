9d86f0dee3d0c04612378f299e64c576d40de039
Fraser Ard <fraserard@gmail.com>
2021-07-19 18:47:35 -0600

    updated players api to use jwt

--- api/statagems/routes/players_resource.py
+++ api/statagems/routes/players_resource.py
@@ -1,30 +1,36 @@
 from flask import request
+from flask_jwt_extended.view_decorators import jwt_required
 from flask_restful import Resource
 
-from statagems.models import db
-from statagems.models.player import Player 
-from statagems.schemas.player_schemas import players_schema, player_schema
+
+from ..extensions import db
+from ..models.player import Player 
+from ..schemas.player_schemas import players_schema, player_schema
 
 class PlayersApi(Resource): # /api/players
+    @jwt_required()
     def get(self): # get all players
         players = Player.query.all()
         return players_schema.dump(players)
-        
+    @jwt_required()    
     def post(self): # add a player
         data = request.get_json()
         new_player = Player(
             steam_id = data['steam_id'],
             username = data['username'],
             preferred_username = data['preferred_username'],
-            name = data['name'])
+            name = data['name'],
+            profile_pic_url = data['profile_pic_url'])
         db.session.add(new_player)
         db.session.commit()
         return {"msg": "success"}
 
 class PlayerApi(Resource): # /api/players/<id>
+    @jwt_required()
     def get(self, id): # get a player by id
         player = Player.query.get_or_404(id)
         return player_schema.dump(player)
+    @jwt_required()
     def put(self, id): # update player by id
         data = request.get_json()
         player = Player.query.get(id)
@@ -39,7 +45,7 @@ class PlayerApi(Resource): # /api/players/<id>
             player.name = data['name']
         db.session.commit()
         return {"msg": "success"}
-        
+    @jwt_required()    
     def delete(self, id): # delete a player by id
         player = Player.query.get(id)
         if player is None:
