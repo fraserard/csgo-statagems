4766d0c9e05053bc153a1843538aa55d3d353170
Fraser Ard <fraserard@gmail.com>
2021-08-16 13:35:46 -0600

    feat: group update/delete

--- api/statagems/helpers/group_helper.py
+++ api/statagems/helpers/group_helper.py
@@ -1,5 +1,6 @@
 import logging
-from ..models import Group, GroupPlayer
+
+from ..models import Group, GroupPlayer, Player
 from ..extensions import db
 
 def get_groups_for_player(pid):
@@ -9,29 +10,51 @@ def create_group(creator_id: int, group_data: dict):
     """Creates new group under Player's id
     
     :param creator_id: id of Player who is creating group
-    :param group_data: dict of group data
-    :return: player_data json if succcess, else raise Exception."""
+    :param group_data: dict of group data"""
 
-    try:
-        new_group = Group(
-            group_name = group_data['group_name'],
-            description = None if 'description' not in group_data else group_data['description'],
-            creator_id = creator_id,
-        )
-        db.session.add(new_group)
-        db.session.flush()
-        logging.error(new_group)
-        # auto create groupplayer on group create
-        group_member = GroupPlayer(
-            player_id = creator_id,
-            group_id = new_group.id,
-            group_clearance = 0,     
-        )
-        logging.error(group_member)
-        db.session.add(group_member)
-        db.session.commit()
-    except Exception as e: raise e
+   
+    new_group = Group(
+        group_name = group_data['group_name'],
+        description = None if 'description' not in group_data else group_data['description'],
+        creator_id = creator_id,
+    )
+    db.session.add(new_group)
+    db.session.flush()
     
+    # auto create groupplayer on group create
+    group_member = GroupPlayer(
+        player_id = creator_id,
+        group_id = new_group.id,
+        group_clearance = 0,     
+    )
+    
+    db.session.add(group_member)
+        
+    
+def update_group(pid: int, group_data: dict, gid: int):
+    """Updates group with data, if user has permissions
+
+    :param pid: id of Player who is updating Group
+    :param group_data: dict of Group data
+    :param gid: id of Group to update"""
+
+    g = Group.query.get(gid)
+    if g is None: raise Exception('Group does not exist')
+    gp = GroupPlayer.query.filter(GroupPlayer.player_id == pid and GroupPlayer.group_id == gid)
+    if gp.group_clearance not in [0,1,2]: raise Exception('Player does not have permissions to update the group')
+    
+    if 'group_name' in group_data:
+        g.group_name = group_data['group_name']
+    if 'description' in group_data:
+        g.description = group_data['description']
+
+def delete_group(pid: int, gid: int):
+    g = Group.query.get(gid)
+    if g is None: raise Exception('Group does not exist')
+    gp = GroupPlayer.query.filter(GroupPlayer.player_id == pid and GroupPlayer.group_id == gid)
+    if gp.group_clearance not in [0,1]: raise Exception('Player does not have permissions to update the group')
+
+    db.session.delete(g)
 
 def add_group_member():
     NotImplemented
--- api/statagems/routes/group_resource.py
+++ api/statagems/routes/group_resource.py
@@ -1,48 +1,85 @@
 
 import logging
+from flask_jwt_extended.view_decorators import jwt_required
 from flask_restful import Resource
 from flask_jwt_extended.utils import get_jwt_identity
 from flask import request, make_response
 from sqlalchemy.orm import load_only
-from ..helpers.group_helper import create_group
+from ..helpers.group_helper import create_group, update_group, delete_group
 from ..extensions import db
 from ..models.player import Player
 from ..models.group import Group
 from ..models.group_player import GroupPlayer
 from ..schemas.group_schemas import GroupSchema
+from api.statagems.models import group
 
-class GroupsApi(Resource): # /api/groups  
+class GroupsApi(Resource): # /api/groups 
+    @jwt_required() 
     def get(self): # get groups for specific player
-        player = get_jwt_identity()
-        pid = player['id']
+        try:
+            player = get_jwt_identity()
+            pid = player['id']
         
-        groups = db.session.query(Group).join(Group._members).filter(GroupPlayer.player_id == pid)
-  
-        return GroupSchema(many=True).dump(groups)
-
+            groups = db.session.query(Group).join(Group._members).filter(GroupPlayer.player_id == pid)
+        except Exception as e:
+            logging.exception(e)
+            return 400
+        
+        return GroupSchema(many=True).dump(groups), 200
+    @jwt_required()
     def post(self): # add new group
+        data = request.get_json(silent=True)
+        if data is None: return 400
         try:
             data = request.get_json()
             player = get_jwt_identity()
             pid = player['id'] 
 
-            create_group(pid, data)
+            create_group(creator_id=pid, group_data=data)
 
             db.session.commit()
         except Exception as e:
             logging.exception(e)
-            return {'msg': 'error'}, 400
-        except BaseException as e:
-            logging.exception(e)
-            return {'msg': 'error'}, 400
+            return 400
         
-        return {'msg': 'success'}, 201
+        return 201
 
 class GroupApi(Resource): # /api/groups/<id>
-    def get(self): # get group by id
-        
-        return NotImplemented
+    @jwt_required()
+    def get(self, id): # get group by groupid
+        group = Group.query.get_or_404(id)
+        return GroupSchema().dump(group), 200
 
-    def put(self): # update group
+    @jwt_required()
+    def put(self, id): # update group by groupid
         
-        return NotImplemented
+        data = request.get_json(silent=True)
+        if data is None: return 400
+        try:
+            data = request.get_json()
+            player = get_jwt_identity()
+            pid = player['id'] 
+
+            update_group(pid=pid, group_data=data, gid=id)
+
+            db.session.commit()
+        except Exception as e:
+            logging.exception(e)
+            return 400
+        
+        return 204
+
+    @jwt_required()
+    def delete(self, id): # delete group by groupid
+        try:
+            player = get_jwt_identity()
+            pid = player['id'] 
+
+            delete_group(pid=pid, gid=id)
+
+            db.session.commit()
+        except Exception as e:
+            logging.exception(e)
+            return 400
+        
+        return 204
