79a87b221017f5c2c06008c6e87acca56627c85f
Fraser Ard <fraserard@gmail.com>
2021-08-16 09:57:50 -0600

    changed group created and updated fields to type timestamp

--- api/statagems/models/group.py
+++ api/statagems/models/group.py
@@ -10,8 +10,8 @@ class Group(db.Model): # groups, users can join
     description = db.Column(db.String(255), nullable=True) # description of group
     creator_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False) # user id of group creator, CREATE INDEX?
 
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, server_default=db.text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"), server_onupdate=db.FetchedValue())
+    created_at = db.Column(db.TIMESTAMP, server_default=db.func.now())
+    updated_at = db.Column(db.TIMESTAMP, server_default=db.text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"), server_onupdate=db.FetchedValue())
 
     creator = db.relationship('Player', back_populates='created_groups') # a group has one creator
     matches = db.relationship('Match', back_populates='group') # a group can have many matches
@@ -20,4 +20,4 @@ class Group(db.Model): # groups, users can join
     members = association_proxy('_members', 'player', creator=lambda _p: GroupPlayer(player=_p))
     
     def __repr__(self):
-        return f'{self.__class__.__name__}<steamId: {self.group_name}>'
+        return f'{self.__class__.__name__}<Id: {self.id}, name: {self.group_name}>'
