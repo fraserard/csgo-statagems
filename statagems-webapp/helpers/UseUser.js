8851f3c1f4e5148a49d48e90827d84f9dbdb7ba8
Fraser Ard <fraserard@gmail.com>
2021-08-20 13:45:07 -0600

    cleaned up userfetcher

--- statagems-webapp/helpers/UseUser.js
+++ statagems-webapp/helpers/UseUser.js
@@ -1,6 +1,7 @@
 import useSWR, {cache} from "swr"
-import {userFetcher} from "./UserFetcher"
-  
+import { server } from "../config";
+import cookieCutter from 'cookie-cutter'
+
 export default function useUser() {
     
     const options = {
@@ -12,3 +13,22 @@ export default function useUser() {
         user: data,      
     }
 }
+
+export const userFetcher = async () => {
+    
+    const options = {
+        headers: {
+        'X-CSRF-TOKEN': cookieCutter.get('csrf_access_token')
+        }
+    }
+    const resp = await fetch(`${server}/api/me`, options)
+    
+    const data = await resp.json()
+    if (resp.status == 200 && data != undefined && data.id != undefined) {
+
+        return data
+    }
+
+    // not authorized
+    return data
+  }
