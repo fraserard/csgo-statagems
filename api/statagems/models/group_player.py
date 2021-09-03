7d87c2ee36b6a68a7d2ba2b6a0a105bcfa7485ad
Fraser Ard <fraserard@gmail.com>
2021-09-02 20:41:37 -0600

    set group player default clearance to 5

--- api/statagems/models/group_player.py
+++ api/statagems/models/group_player.py
@@ -6,7 +6,7 @@ class GroupPlayer(db.Model): # player of group
  
     player_id = db.Column(db.Integer, db.ForeignKey('player.id'), primary_key=True, autoincrement=False) # player id, pk
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'), primary_key=True, autoincrement=False) # group id, pk
-    group_clearance = db.Column(db.Integer, nullable=False) # player permissions in group
+    group_clearance = db.Column(db.Integer, default=5, nullable=False) # player permissions in group
     
     player = db.relationship('Player', back_populates='_groups') # a Player is part of many Groups
     group = db.relationship('Group', back_populates='_members') # a Group can have many Members
